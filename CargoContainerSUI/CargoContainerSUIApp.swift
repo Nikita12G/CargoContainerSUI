ca19651c086ec4b3a5f6b7f4aaf0dc698981226e
Nikita12G <gulyi08@mail.ru>
2024-07-27 12:05:30 +0700

    Initial Commit

---
+++ CargoContainerSUI/CargoContainerSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CargoContainerSUIApp.swift
+//  CargoContainerSUI
+//
+//  Created by Никита Гуляев on 27.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CargoContainerSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
